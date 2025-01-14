fb788703b86b65de42101c33133e9e72558efa92
Zomb42 <dericks1real@gmail.com>
2025-01-13 23:30:38 -0500

    backend start

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+//const express = require('express');
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready123");
+
+
+});
+
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000");
+});
