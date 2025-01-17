89fe20892a2fa2285095f1ff7a6943d551f23b14
Zomb42 <dericks1real@gmail.com>
2025-01-17 11:43:38 -0500

    added database

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 //const express = require('express');
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 
+dotenv.config()
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready123");
+app.get("/products", (req, res) => {});
 
-
-});
-
+console.log(process.env.MONGO_URI);
 
 app.listen(3000, () => {
+    connectDB();
     console.log("Server started at http://localhost:3000");
 });
+
+
