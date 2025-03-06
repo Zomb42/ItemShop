7860225e230c13994010c1c3205d04a1ceedfaf3
Zomb42 <dericks1real@gmail.com>
2025-03-06 00:06:40 -0500

    fixed front end bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react"
+//import './App.css'
 
 function App() {
-    return(
-      <>
-        <Button>Hello</Button>
-      
-      </>
-    );
+  return(
+    <>
+    <Button>Hello good person</Button>
+    </>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from "@chakra-ui/react"
+import { system } from "@chakra-ui/react/preset";
+//import './index.css'
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-    <App/>
+    <ChakraProvider value={system} >
+    <App />
     </ChakraProvider>
+    
   </StrictMode>,
 )
